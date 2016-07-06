c6828e82e7523a5b55764c0ae33fdb08ce65aaac
alisan dagdelen <alisandagdelen@gmail.com>
2016-07-07 01:17:34 +0300

    Add POST /todos

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch!',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Work more!',
-	completed: true
-}]
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 app.get('/', function(req , res){
 	res.send('TODO App !! Root');
 });
-
+// GET /todos
 app.get('/todos', function(req, res){
 	res.json(todos);
 })
+// GET /todos/:d 
 app.get('/todos/:id',function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo;
@@ -35,8 +29,15 @@ app.get('/todos/:id',function(req, res){
 	}else{
 		res.status(404).send();
 	}
-	//res.status(404).send();
 	res.send('Asking for todo with id of ' + req.params.id)
+});
+
+// POST /todos
+app.post('/todos',function(req , res){
+	var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
+	res.json(body);
 });
 
 app.listen(PORT , function(){
