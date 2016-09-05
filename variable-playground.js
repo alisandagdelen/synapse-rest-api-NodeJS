447118e1ebfe6d357ca22cc97f9ad13106600629
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-05 16:33:04 +0200

    add variable-playground

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+/*var person = {
+	name :'Alisan',
+	age: 21
+};
+function updatePerson(obj){
+	obj = {
+		name: 'Alisan',
+		age: 24
+	};
+
+	obj.age = 24;
+
+}
+updatePerson(person);
+console.log(person);	
+*/
+//Array Example
+var grades = [15,88];
+function addGrades (gradesArr) { 
+	gradesArr.push(55);	
+	debugger;
+	
+}
+
+addGrades(grades);
+console.log(grades);
