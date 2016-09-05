da8dfc699e2dd07aab2e25958e8865d5ec967a3d
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-05 13:31:36 +0200

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -17,12 +18,13 @@ app.get('/todos', function(req, res){
 // GET /todos/:d 
 app.get('/todos/:id',function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	/*var matchedTodo;
 	todos.forEach(function(todo){
 		if (todoId === todo.id){
 			matchedTodo = todo;
 		}
-	});
+	});*/
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -34,7 +36,13 @@ app.get('/todos/:id',function(req, res){
 
 // POST /todos
 app.post('/todos',function(req , res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	//set body.descripton to be trimed value
     body.id = todoNextId++;
     todos.push(body);
 	res.json(body);
