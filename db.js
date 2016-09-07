9d98b7f411030c51c7dcfb27dce21c862dd71a9f
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-07 16:46:05 +0200

    big fix

--- db.js
+++ db.js
@@ -1,30 +1,15 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
-var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
-
-if (env === 'production') {
-	sequelize = new Sequelize(process.env.DATABASE_URL, {
+if (env === 'production'){
+	sequelize = new Sequelize(process.env.DATABASE_URL,{
 		dialect: 'postgres'
 	});
-} else {
-	var sequelize = new Sequelize(undefined, undefined, undefined, {
-		'dialect': 'sqlite',
-		'storage': __dirname + '/data/dev-todo-api.sqlite'
-	});
+}else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': __dirname + '/data/dev-todo-api.sqlite'
 }
 /*
 var sequelize = new Sequelize(undefined, undefined, undefined, {
