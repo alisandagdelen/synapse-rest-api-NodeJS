c814e563bd5d263256bb62dfb7104086f882d5bd
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-07 13:18:34 +0200

    todos id from sqlite added

--- server.js
+++ server.js
@@ -40,15 +40,25 @@ app.get('/todos', function(req, res) {
 	// GET /todos/:d 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
+	db.todo.findById(todoId).then(function(todo){
+		if(!!todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	},function(e){
+		res.status(500).send();
+	});
+	/*
 	var matchedTodo = _.findWhere(todos, {
 		id: todoId
 	});
-	/*var matchedTodo;
+	var matchedTodo;
 	todos.forEach(function(todo){
 		if (todoId === todo.id){
 			matchedTodo = todo;
 		}
-	});*/
+	});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -56,27 +66,28 @@ app.get('/todos/:id', function(req, res) {
 		res.status(404).send();
 	}
 	res.send('Asking for todo with id of ' + req.params.id)
+	*/
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	db.todo.create(body).then(function(todo){
+	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
-	},function(e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
-/*
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-	body.description = body.description.trim();
-	//set body.descripton to be trimed value
-	body.id = todoNextId++;
-	todos.push(body);
-	res.json(body);
-	//try line  console.log('Deneme : ' + body.hasOwnProperty('completed') + '  weqweq  ' + body.description);
-	*/
+	/*
+		if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+			return res.status(400).send();
+		}
+		body.description = body.description.trim();
+		//set body.descripton to be trimed value
+		body.id = todoNextId++;
+		todos.push(body);
+		res.json(body);
+		//try line  console.log('Deneme : ' + body.hasOwnProperty('completed') + '  weqweq  ' + body.description);
+		*/
 });
 
 // DELETE /todos/:id
