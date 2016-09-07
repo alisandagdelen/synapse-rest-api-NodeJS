cee2dc15f68be82f8e52675480ece2e033270f50
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-07 16:48:59 +0200

    big fix

--- db.js
+++ db.js
@@ -2,14 +2,15 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if (env === 'production'){
-	sequelize = new Sequelize(process.env.DATABASE_URL,{
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		dialect: 'postgres'
 	});
-}else {
+} else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
 /*
 var sequelize = new Sequelize(undefined, undefined, undefined, {
