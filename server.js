97f6ed46eb670a902341ba0abb607a567e8440a1
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-09 13:35:37 +0200

    Ad auth middleware and findById token

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.get('/', function(req, res) {
 	res.send('TODO App !! Root');
 });
 // GET /todos?completed=true
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 		var query = req.query;
 		var where = {};
 
@@ -60,7 +61,7 @@ app.get('/todos', function(req, res) {
 		*/
 	})
 	// GET /todos/:d 
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id',middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	db.todo.findById(todoId).then(function(todo) {
 		if (!!todo) {
@@ -92,7 +93,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos',middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
@@ -113,7 +114,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id',middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	db.todo.destroy({
 		where: {
@@ -146,7 +147,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id',middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	/*
 	var matchedTodo = _.findWhere(todos, {
