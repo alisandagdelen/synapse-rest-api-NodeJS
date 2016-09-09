e48c451b2cd6a75e75c4a7983cd0d4499e4038c2
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-09 12:36:37 +0200

    Add auth token generation to post user/login

--- server.js
+++ server.js
@@ -183,28 +183,36 @@ app.put('/todos/:id', function(req, res) {
 	})
 });
 
-app.post('/users' , function(req, res){
+app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	db.user.create(body).then(function(user){
+	db.user.create(body).then(function(user) {
 		res.json(user.toPublicJSON());
-	},function(e){
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
 // POST /user/login
-app.post('/users/login', function(req, res){
+app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	db.user.authenticate(body).then(function(user) {
+		var token = user.generateToken('authentication');
+		if (token) {
+			res.header('Auth', token).json(user.toPublicJSON());
 
-	db.user.authenticate(body).then(function(user){	
-		res.json(user.toPublicJSON());
-	},function(){
+		} else {
+			res.status(401).send();
+
+		}
+	}, function() {
 		res.status(401).send();
 	});
-	
+
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port !' + PORT + '!');
 	});
