776ea9d824b594909d2ccb8fdf6b9b8c2cd1b364
alisan dagdelen <alisandagdelen@gmail.com>
2016-09-24 00:04:02 +0200

    todo variables changed

--- models/todo.js
+++ models/todo.js
@@ -1,18 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('todo', {
-		description: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				len: [1, 250]
-			}
-
-		},
 		title: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
 				len: [1, 100]
+			}
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 250]
 			}
 
 		},
